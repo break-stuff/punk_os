3ed9b0eec84cc8de5430a39e6985c71b94a0726b
Burton Smith <burton@breakstuff.io>
2025-11-03 19:47:17 -0500

    add variable check

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@
 ###############################################################################
 
 set -e  # Exit immediately on error (because we're dramatic like that)
+set -o nounset  # Treat unset variables as errors
 
 show_logo() {
     echo "                            @@@@@  "
